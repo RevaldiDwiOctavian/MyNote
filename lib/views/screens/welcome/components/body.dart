195be673c388bb2fac930ab9dbafddfeaaecba07
Revaldi Dwi Octavian <revaldidwioctavian@gmail.com>
2022-07-16 13:33:37 +0700

    Adding welcome screen

---
+++ lib/views/screens/welcome/components/body.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uas/constant.dart';
+
+import 'background.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Welcome to MyNotes",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: SvgPicture.asset(
+                "assets/icons/chat.svg",
+                height: size.height * 0.45,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  primary: Colors.white,
+                  backgroundColor: kPrimaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  minimumSize: Size(size.width * 1, size.height * 0.01),
+                ),
+                onPressed: () {},
+                child: Text("LOGIN"),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  primary: Colors.white,
+                  backgroundColor: kPrimaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  minimumSize: Size(size.width * 1, size.height * 0.01),
+                ),
+                onPressed: () {},
+                child: Text("SIGNUP"),
+              ),
+            )
+          ]),
+    );
+  }
+}
